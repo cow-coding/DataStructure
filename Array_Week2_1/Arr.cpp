5379b410aa1290e82416e99d7b2584ecbe104431
Ki-Bum Park <61653740+cow-coding@users.noreply.github.com>
2020-03-13 19:28:11 +0900

    Update Arr.cpp

--- Array_Week2_1/Arr.cpp
+++ Array_Week2_1/Arr.cpp
@@ -1,51 +1,104 @@
 #include <iostream>
 using namespace std;
-
-class Array {
+class Arr {
 private:
-    int* arr; // 동적할당을 구현할 본체 포인터 입니다.
-    int size; // 전체 배열의 크기를 받아 올 변수입니다.
-
+    int* arr;
+    int size;
+    
 public:
-    Array(int s) {
+    Arr(int s) {
         this -> size = s;
         
-        arr = new int[size];
-
-        for(int i = 0; i < size; i++)
+        arr = new int[s];
+        
+        // 배열 생성과 동시에 초기화
+        for (int i = 0; i < size; i++)
             arr[i] = 0;
     }
-
-    int at(int i) {
-        if(arr[i] == 0)
-            return 0;
-        else
-            return arr[i];
-    }
-
-    void set(int i, int X) {
-        if(arr[i] == 0)
-            cout << 0 << "\n";
-        else
-            arr[i] = X;
-    }
-
+    
+    // 배열이 비었는 지 확인한다.
     bool isEmpty() {
-        if(arr[0] == 0)
+        if (arr[0] == 0)
             return true;
-        else
-            return false;
+        
+        return false;
     }
-
-    void printArray() {
-        if(isEmpty())
+    
+    int at(int idx) {
+        if (arr[idx] == 0)
+            return 0;
+            //cout << 0 << "\n";
+        else
+            return arr[idx];
+            //cout << arr[idx] << "\n";
+    }
+    
+    void set(int idx, int X) {
+        if (arr[idx] == 0)
             cout << 0 << "\n";
         else {
-            for(int i = 0; i < this -> size; i++) {
-                if(arr[i] == 0)
-                     break;
-            cout << arr[i] << " ";
+            arr[idx] = X;
         }
-        cout << "\n";
     }
-}
+    
+    void add(int idx, int X) {
+    	if(arr[idx] == 0) { // 이건 arr가 빈 경우도 해당한다.
+        	for(int i = 0; i < this -> size; i++) {
+            	if(arr[i] == 0) {
+                	arr[i] = X;    
+                	break;
+            	}
+        	}
+    	} else {
+        	int* tmp = new int[this -> size];
+        
+        	for(int i = 0; i < this -> size; i++)
+            	tmp[i] = arr[i];
+
+        	for(int i = idx; i < size - 1; i++) {
+            	tmp[i + 1] = arr[i];
+            
+            	if(arr[i + 1] == 0)
+                	break;
+        	}
+        	tmp[idx] = X;
+        	arr = tmp;
+    	}
+	}
+    
+    void remove(int idx) {
+    if(arr[idx] == 0)
+        cout << 0 << "\n";
+    else {
+        int* tmp = new int[size];
+        
+        for(int i = 0; i < size; i++)
+            tmp[i] = arr[i];
+
+        cout << arr[idx] << "\n";
+
+        for(int i = idx; i < size - 1; i++) {
+            if(arr[i + 1] == 0) {
+                tmp[i] = 0;
+                break;
+            }
+            tmp[i] = arr[i + 1];
+        }
+        arr = tmp;
+    }
+    
+    void printArray() {
+        if (isEmpty())
+            cout << 0 << "\n";
+        else {
+            for (int i = 0; i < this -> size; i++) {
+                if (arr[i] == 0)
+                    break;
+                
+                cout << arr[i] << " ";
+            }
+            cout << "\n";
+        }
+    }
+};
+
